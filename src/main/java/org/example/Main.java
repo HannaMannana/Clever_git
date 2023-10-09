4ed684fbd0e8309bcbd2d25df8f19401dcad5489
Ann_Mikhaliova <annmikhaleva35@gmail.com>
2023-10-09 22:46:13 +0300

    feat:22

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-        int b = 2;
+        int b = 22;
     }
 }
